bf55a489d75f3a22c7b5b42b38b278b8bcf698e6
Onn <otabekteshabaev07@gmail.com>
2021-04-03 15:50:54 +0600

    end

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static String adamAndEva="Салам Алейкум Братья!";
+    public final static int NUM = 5;
+    public static String word = "привет";
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println(NUM + word);
+
+
+
+    }
+}
